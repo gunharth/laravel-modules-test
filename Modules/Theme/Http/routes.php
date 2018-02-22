b4a358eee3267e7fa6009a438d60d443f43b2d2e
Gunharth Randolf <hello@gunharth.io>
2018-02-22 18:34:29 +0100

    Cleanup Module Theme folder

--- Modules/Theme/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'theme', 'namespace' => 'Modules\Theme\Http\Controllers'], function()
-{
-    Route::get('/', 'ThemeController@index');
-});
